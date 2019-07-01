27e91019782d1f44c288851a8b56c123eb0f2b97
Jasper Schelling <jasper@jasperschelling.com>
2019-07-01 11:40:57 +0200

    Update subnav.php
    added flexclasses

--- site/snippets/subnav.php
+++ site/snippets/subnav.php
@@ -1,10 +1,10 @@
-<ul id="subnav" class="nav bg-white shadow-sm nav-underline ">
+<ul id="subnav" class="nav bg-white shadow-sm nav-underline flex-column flex-md-row">
   <li class="nav-item">
-    <a class=" nav-link <?php if($page->isActive() === true): echo ' active'; endif ?>" href="<?= $page->url() ?>">Introductie</a>
+    <a class="flex-md-fill text-md-center nav-link <?php if($page->isActive() === true): echo ' active'; endif ?>" href="<?= $page->url() ?>">Introductie</a>
   </li>
   <?php foreach($items as $item): ?>
   <li class="nav-item">
-    <a class="nav-link<?php if($item->isActive() === true): echo ' active'; endif ?>" href="<?= $item->url() ?>"><?= html($item->title()) ?></a>
+    <a class="flex-md-fill text-md-center nav-link<?php if($item->isActive() === true): echo ' active'; endif ?>" href="<?= $item->url() ?>"><?= html($item->title()) ?></a>
   </li>
 <?php endforeach ?>
 </ul>
