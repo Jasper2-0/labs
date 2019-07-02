3b329837d79cfe19c0c39d48b1d0e7a0420b9ca4
Jasper Schelling <jasper@jasperschelling.com>
2019-07-02 22:36:45 +0200

    snippet cleanup

--- site/snippets/com-dropdown.php
+++
@@ -1,8 +0,0 @@
-  <a class="<?= $class ?> <?= e($disabled == true,'disabled','')?>" data-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="false"><?= $value ?></a>  
-  <? if(!$disabled): ?>
-  <div class="dropdown-menu">
-    <?php foreach($items as $item): ?>
-    <a class="dropdown-item" href="<?= $item->url() ?>"><?= $item->title()->html() ?></a>
-    <?php endforeach ?>
-  </div>
-  <? endif ?>
